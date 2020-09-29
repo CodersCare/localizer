2604e76a71a1a1e89d823c0fc0d728f249aa2c11
cybercraft <info@cybercraft.de>
2020-09-29 14:32:52 +0200

    [BUGFIX] Exclude mandatory table pages from selector dropdown and matrix

--- Classes/Controller/SelectorController.php
+++ Classes/Controller/SelectorController.php
@@ -610,6 +610,9 @@ class SelectorController extends AbstractController
             '</a></li>';
         $this->translatableTables = ['pages' => $GLOBALS['LANG']->sL($GLOBALS['TCA']['pages']['ctrl']['title'])];
         foreach (array_keys($GLOBALS['TCA']) as $table) {
+            if ($table === 'pages') {
+                continue;
+            }
             $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
             $queryBuilder->getRestrictions()
                 ->removeAll()
@@ -714,6 +717,9 @@ class SelectorController extends AbstractController
         $translationLocalizer .= '</tr></thead><tbody>';
         $this->tableHeaderSpan = count($this->languages) + 2;
         foreach ($this->translatableTables as $table => $title) {
+            if ($table === 'pages') {
+                continue;
+            }
             $labelField = $GLOBALS['TCA'][$table]['ctrl']['label'];
             if (!empty($this->data['records'][$table])) {
                 $counter = 0;
