441b631d334d5ddef17a1afd81ff9508f56a4716
Jo Hasenau <info@cybercraft.de>
2022-01-17 17:21:29 +0100

    [TASK] Make sure to fetch locales based on site configuration

--- Classes/Model/Repository/LanguageRepository.php
+++ Classes/Model/Repository/LanguageRepository.php
@@ -4,27 +4,17 @@ namespace Localizationteam\Localizer\Model\Repository;
 
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Site\SiteFinder;
-use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class LanguageRepository extends AbstractRepository
 {
-    public function getIsoTwoCodeByLocale(string $locale, int $pid = 1): string
+    public function getIsoTwoCodeByTargetLocaleId(int $localeId, int $pid = 1): string
     {
-        $locale = str_replace('-', '_', $locale);
         $site = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId($pid);
         $languages = $site->getAllLanguages();
 
-        DebugUtility::debug($languages);
-        $filteredLanguages = array_filter($languages, function (SiteLanguage $language) use ($locale) {
-            return strpos($language->getLocale(), $locale) !== false;
-        });
-
-        /** @var SiteLanguage $language */
-        $language = array_shift($filteredLanguages);
-
-        if ($language instanceof SiteLanguage) {
-            return mb_strtoupper($language->getTypo3Language());
+        if ($languages[$localeId] instanceof SiteLanguage) {
+            return $languages[$localeId]->getHreflang();
         }
 
         return '';
