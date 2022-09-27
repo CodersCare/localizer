38e88139c7a88f46a366621bc945238b3fc2f59a
Jo Hasenau <info@cybercraft.de>
2022-09-27 11:41:32 +0200

    [TASK] Remove deprecated modules

--- Modules/Localizer/Cart/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Localizationteam\Localizer\Controller\CartController;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-$GLOBALS['SOBE'] = GeneralUtility::makeInstance(CartController::class);
-$GLOBALS['SOBE']->init();
-$GLOBALS['SOBE']->main();
-$GLOBALS['SOBE']->printContent();
--- Modules/Localizer/Selector/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Localizationteam\Localizer\Controller\SelectorController;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-$GLOBALS['SOBE'] = GeneralUtility::makeInstance(SelectorController::class);
-$GLOBALS['SOBE']->init();
-$GLOBALS['SOBE']->main();
-$GLOBALS['SOBE']->printContent();
