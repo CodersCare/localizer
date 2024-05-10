52887939e045663bb9dd3303fe3b0b57c30d19be
Stefano Kowalke <info@arroba-it.de>
2024-05-10 14:09:58 +0200

    Update LocalizerController

--- Classes/Controller/LocalizerController.php
+++ Classes/Controller/LocalizerController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Localizationteam\Localizer\Controller;
 
+use TYPO3\CMS\Backend\Attribute\Controller;
+
 /**
  * Module 'Localizer' for the 'l10n_matrix' extension.
  *
  * @author      Peter Russ<peter.russ@4many.net>, Jo Hasenau<jh@cybercraft.de>
+ * @todo With the new ModuleAPI, this class is not needed anymore and can be removed when dropping support for v11.
  */
+#[Controller]
 class LocalizerController extends BaseModule
 {
     public function main(): void
